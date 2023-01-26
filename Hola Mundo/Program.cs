b9ef8c18943cc987da7ace483d55dd349ec44371
LeoCR13 <123657425+LeoCR13@users.noreply.github.com>
2023-01-26 13:20:25 -0400

    Add files via upload

---
+++ Hola Mundo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hola_Mundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Leonardo por aqui");
+            Console.Read();
+        }
+    }
+}
